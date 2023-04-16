62007f908ffda474403f10c70a92c0db8901be60
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-04-15 21:41:34 -0400

    logged in features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './_services/user.service';
+import { TokenStorageService } from './_services/token-storage.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'paapp2';
+  content?: string;
+  currentUser: any;
+  isLoggedIn = false;
+  showAdmin = false;
+  showUser = false;
+  showMod = false;
 
+  private roles: string[] = [];
+  
+
+  message = '';
+
+  constructor(
+    private userService: UserService, 
+    private router: Router, 
+    private  token: TokenStorageService, 
+    ) { }
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.token.getToken();
+    this.currentUser = this.token.getUser();
+
+    if (this.isLoggedIn) {
+      const user = this.token.getUser();
+      this.roles = user.roles;
+      console.log(user)
+      this.showAdmin = this.roles.includes('ROLE_ADMIN');
+      this.showMod = this.roles.includes('ROLE_MODERATOR');
+      this.showUser = true
+    }
+  }
   menuOpen = false;
 
   toggle() {
@@ -26,6 +58,15 @@ export class AppComponent {
 
     //or document.body.scrollTop = 0;
     //or document.querySelector('body').scrollTo(0,0)
+
+  }
+
+  logout(): void {
+    this.token.signOut();
+    this.router.navigate(['/home']);
+    setTimeout(() => {
+      window.location.reload();
+    }, 10);
 
   }
 
