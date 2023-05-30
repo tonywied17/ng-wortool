eb7b308b11025030421167669a6ecdef2269f61a
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-05-29 22:27:52 -0400

    more map-details component work

--- src/app/map-details/map-details.component.ts
+++ src/app/map-details/map-details.component.ts
@@ -66,4 +66,18 @@ export class MapDetailsComponent implements OnInit {
     }
   }
 
+  /**
+   * 
+   * @param url 
+   * @param title 
+   * @param w 
+   * @param h 
+   * @returns 
+   */
+  open(url: any, title: any, w: any, h: any) {
+    var left = (screen.width / 2) - (w / 2);
+    var top = (screen.height / 2) - (h / 2);
+    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+  }
+
 }
