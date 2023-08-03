fe144a5e483e97399533bcd35e4e93f82ff5a9a3
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-08-03 19:30:22 -0400

    production sw for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
  * Created Date: Sunday July 2nd 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Thu August 3rd 2023 7:17:13 
+ * Last Modified: Thu August 3rd 2023 7:30:07 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
@@ -103,8 +103,8 @@ const routes: Routes = [
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
     ServiceWorkerModule.register("ngsw-worker.js", {
-      enabled: !isDevMode(),
-      // enabled: environment.production,
+      // enabled: !isDevMode(),
+      enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: "registerWhenStable:30000",
