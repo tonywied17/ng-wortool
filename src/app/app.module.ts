fd39d3b9e9738c415b911c556f2d2757107df9d9
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-10-11 16:26:59 -0400

    error event for avatars not loading will set avatar as placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
  * Created Date: Sunday July 2nd 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Wed August 9th 2023 2:09:40 
+ * Last Modified: Wed October 11th 2023 3:38:39 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
@@ -103,8 +103,8 @@ const routes: Routes = [
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
     ServiceWorkerModule.register("ngsw-worker.js", {
-      // enabled: !isDevMode(),
-      enabled: environment.production,
+      enabled: !isDevMode(),
+      // enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: "registerWhenStable:30000",
