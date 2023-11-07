ef9e6aadba4efcb64c44c95737adaaa3b7becef8
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-11-06 20:30:45 -0500

    some mobile ui fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
  * Created Date: Sunday July 2nd 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Mon November 6th 2023 8:00:36 
+ * Last Modified: Mon November 6th 2023 7:37:44 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
