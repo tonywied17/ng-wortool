203e9facd6dba56c028f0d97c666a16814a45430
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2024-02-11 16:49:17 -0500

    nav hide on scroll fix, scroll to top fix, page titles margin fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
  * Created Date: Sunday July 2nd 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Sun February 11th 2024 4:19:36 
+ * Last Modified: Sun February 11th 2024 4:48:40 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
