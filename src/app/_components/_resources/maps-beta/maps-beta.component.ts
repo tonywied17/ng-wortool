ec476f5225e52e39ad089816d03674d70cfadc90
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2024-02-16 16:53:14 -0500

    user like buttons with toggle functionality and classes applied with badge number

--- src/app/_components/_resources/maps-beta/maps-beta.component.ts
+++ src/app/_components/_resources/maps-beta/maps-beta.component.ts
@@ -1,9 +1,11 @@
 import { Component, HostListener, type OnInit } from "@angular/core";
 import { MapService } from "src/app/_services/map.service";
+import { FavoriteService } from "src/app/_services/favorite.service";
 import { WeaponService } from "src/app/_services/weapon.service";
 import { TokenStorageService } from "src/app/_services/token-storage.service";
 import { SharedDataService } from "src/app/_services/shared-data.service";
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-maps-beta",
@@ -24,35 +26,21 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   ]
 })
 export class MapsBetaComponent implements OnInit {
+
   isLoaded: boolean = false;
-  maps: any;
-
   isDesktop: boolean = true;
-  animationStates: {[index: number]: string} = {};
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.checkDeviceType();
-  }
-  checkDeviceType() {
-    this.isDesktop = window.innerWidth > 768;
-  }
-  onMouseEnter(i: number) {
-    if (this.isDesktop) { 
-      this.animationStates[i] = 'hover';
-    }
-  }
 
-  onMouseLeave(i: number) {
-    if (this.isDesktop) { 
-      this.animationStates[i] = 'default';
-    }
-  }
+  maps: any[] = [];
+  userFavorites: any[] = [];
   
   constructor(
     private mapService: MapService,
     private weaponService: WeaponService,
     private tokenStorageService: TokenStorageService,
-    public sharedDataService: SharedDataService
+    public sharedDataService: SharedDataService,
+    public favoriteService: FavoriteService,
+    private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   async ngOnInit(): Promise<void> {
@@ -73,5 +61,63 @@ export class MapsBetaComponent implements OnInit {
         this.maps = data;
         console.log("Retrieved maps:", this.maps);
     });
+  }
+
+  isFavorite(map: any): boolean {
+    return map.map_favorites.some((fav: { id: Number; }) => fav.id === this.sharedDataService.currentUser.id);
+  }
+
+
+  toggleFavorite(map: any): void {
+    const currentRoute = this.router.url;
+    const isFav = this.isFavorite(map);
+    
+    if (isFav) {
+      this.favoriteService.delete(map.id, this.sharedDataService.currentUser.id).subscribe((data) => {
+        console.log("Removed favorite map:", data);
+        if (map.map_favorite_count > 0) {
+          map.map_favorite_count -= 1;
+        }
+        this.updateMapFavoriteStatus(map, false);
+      });
+    } else {
+      this.favoriteService.createOrUpdate(currentRoute, map.id, this.sharedDataService.currentUser.id, "map").subscribe((data) => {
+        console.log("Added favorite map:", data);
+        map.map_favorite_count += 1;
+        this.updateMapFavoriteStatus(map, true);
+      });
+    }
+  }
+
+  updateMapFavoriteStatus(map: any, isFavorite: boolean): void {
+    const mapIndex = this.maps.findIndex(m => m.id === map.id);
+    if (mapIndex !== -1) {
+      if (isFavorite) {
+        map.map_favorites.push({id: this.sharedDataService.currentUser.id});
+      } else {
+        map.map_favorites = map.map_favorites.filter((fav: { id: Number; }) => fav.id !== this.sharedDataService.currentUser.id);
+      }
+    }
+  }
+
+// Custom animation for map cards
+  animationStates: {[index: number]: string} = {};
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkDeviceType();
+  }
+  checkDeviceType() {
+    this.isDesktop = window.innerWidth > 768;
+  }
+  onMouseEnter(i: number) {
+    if (this.isDesktop) { 
+      this.animationStates[i] = 'hover';
+    }
+  }
+
+  onMouseLeave(i: number) {
+    if (this.isDesktop) { 
+      this.animationStates[i] = 'default';
+    }
   }
 }
