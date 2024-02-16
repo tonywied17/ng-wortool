8df1280c82c1a904e68c266c205d6e94ec12c9d0
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2024-02-16 18:08:25 -0500

    new verbose map filters woo

--- src/app/_components/_resources/maps-beta/maps-beta.component.ts
+++ src/app/_components/_resources/maps-beta/maps-beta.component.ts
@@ -26,12 +26,23 @@ import { ActivatedRoute, Router } from "@angular/router";
   ]
 })
 export class MapsBetaComponent implements OnInit {
+  objectKeys = Object.keys;
 
   isLoaded: boolean = false;
   isDesktop: boolean = true;
 
+  weapons: any[] = [];
   maps: any[] = [];
   userFavorites: any[] = [];
+  uniqueCampaigns: string[] = [];
+  selectedCampaigns: { [key: string]: boolean } = {};
+
+  csaArtillery: boolean = false;
+  usaArtillery: boolean = false;
+  filterFavorites: boolean = false;
+  weaponsGroupedByType: { [key: string]: any[] } = {};
+  selectedWeapons: any = {};
+  filteredMaps: any[] = [];
   
   constructor(
     private mapService: MapService,
@@ -44,29 +55,69 @@ export class MapsBetaComponent implements OnInit {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    await this.sharedDataService
-      .retrieveInitialData()
-      .then(async () => {
-        await this.retrieveMaps();
-        this.isLoaded = true;
-        console.log(this.isLoaded)
-      })
-      .catch((error) => {
-        console.error("Error initializing shared data:", error);
-      });
+    try {
+      await this.sharedDataService.retrieveInitialData();
+      await Promise.all([this.retrieveMaps(), this.retrieveWeapons()]);
+      this.isLoaded = true;
+      console.log("All data is loaded:", this.isLoaded);
+    } catch (error) {
+      console.error("Error during initialization:", error);
+    }
   }
 
   async retrieveMaps(): Promise<void> {
-    this.mapService.getAllMapsVerbose().subscribe((data) => {
-        this.maps = data;
-        console.log("Retrieved maps:", this.maps);
+    return new Promise((resolve, reject) => {
+      this.mapService.getAllMapsVerbose().subscribe({
+        next: (data) => {
+          this.maps = data;
+          this.filteredMaps = this.maps;
+  
+          this.uniqueCampaigns = [...new Set(this.maps.map(map => map.campaign))];
+          this.uniqueCampaigns.forEach(campaign => {
+            this.selectedCampaigns[campaign] = false;
+          });
+          this.sortMaps();
+          resolve();
+        },
+        error: (error) => {
+          console.error("Error retrieving maps:", error);
+          reject(error);
+        }
+      });
     });
   }
+  
+  async retrieveWeapons(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.weaponService.getAll().subscribe({
+        next: (data: any) => {
+          this.weapons = JSON.parse(data);
+          this.groupWeaponsByType();
+          resolve();
+        },
+        error: (error: any) => {
+          console.error("Error retrieving weapons:", error);
+          reject(error);
+        }
+      });
+    });
+  }
+  
+  groupWeaponsByType(): void {
+    const grouped = this.weapons.reduce((acc, weapon) => {
+      if (!acc[weapon.type]) {
+        acc[weapon.type] = [];
+      }
+      acc[weapon.type].push(weapon);
+      return acc;
+    }, {});
 
+    this.weaponsGroupedByType = grouped;
+  }
+  
   isFavorite(map: any): boolean {
     return map.map_favorites.some((fav: { id: Number; }) => fav.id === this.sharedDataService.currentUser.id);
   }
-
 
   toggleFavorite(map: any): void {
     const currentRoute = this.router.url;
@@ -100,6 +151,52 @@ export class MapsBetaComponent implements OnInit {
     }
   }
 
+  filterMaps(): void {
+    this.filteredMaps = this.maps.filter(map => {
+      const matchesCSAArtillery = !this.csaArtillery || map.csa_artillery === this.csaArtillery;
+      const matchesUSAArtillery = !this.usaArtillery || map.usa_artillery === this.usaArtillery;
+      const matchesFavorites = !this.filterFavorites || this.isFavorite(map);
+      const matchesCampaign = !this.uniqueCampaigns.some(campaign => this.selectedCampaigns[campaign]) || this.selectedCampaigns[map.campaign];
+  
+      const matchesWeapons = Object.keys(this.selectedWeapons).every(weaponName => {
+        if (!this.selectedWeapons[weaponName]) return true;
+    
+        return map.usa_regiments.Infantry.concat(map.csa_regiments.Infantry, map.usa_regiments.Artillery, map.csa_regiments.Artillery, map.usa_regiments.Cavalry, map.csa_regiments.Cavalry).some((regiment: { regiment_weaponry: any[]; }) => {
+          return regiment.regiment_weaponry.some((weapon: { weapon_info: { weapon: string; }; }) => weapon.weapon_info.weapon === weaponName);
+        });
+      });
+  
+      return matchesCSAArtillery && matchesUSAArtillery && matchesWeapons && matchesFavorites && matchesCampaign;
+    });
+  }
+  
+  sortDirection: 'ascending' | 'descending' = 'descending'; 
+  sortBy: 'favorites' | 'name' = 'favorites'; 
+  sortMaps(): void {
+    if (this.sortBy === 'favorites') {
+      this.filteredMaps.sort((a, b) => {
+        const diff = a.map_favorite_count - b.map_favorite_count;
+        return this.sortDirection === 'ascending' ? diff : -diff;
+      });
+    } else if (this.sortBy === 'name') {
+      this.filteredMaps.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase(); 
+        if (nameA < nameB) {
+          return this.sortDirection === 'ascending' ? -1 : 1;
+        }
+        if (nameA > nameB) {
+          return this.sortDirection === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+  }
+  
+  toggleSortDirection(): void {
+    this.sortDirection = this.sortDirection === 'ascending' ? 'descending' : 'ascending';
+  }
+  
 // Custom animation for map cards
   animationStates: {[index: number]: string} = {};
   @HostListener('window:resize', ['$event'])
